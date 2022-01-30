38bf2ad8db20b4f8812d8cc5eb69c0b89d4e3803
artpumpkin <pumpkin.salah@gmail.com>
2022-01-30 14:03:38 +0100

    feat: 🎸 generate html docs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,40 +1,41 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv'
 
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "hardhat-log-remover";
-import "solidity-coverage";
+import { HardhatUserConfig, task } from 'hardhat/config'
+import '@nomiclabs/hardhat-etherscan'
+import '@nomiclabs/hardhat-waffle'
+import '@typechain/hardhat'
+import 'hardhat-gas-reporter'
+import 'hardhat-log-remover'
+import 'solidity-coverage'
+import 'hardhat-docgen'
 
-dotenv.config();
+dotenv.config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
-task("create-wallet", "Create a new wallet", async (taskArgs, hre) => {
-  const wallet = await hre.ethers.Wallet.createRandom();
+task('create-wallet', 'Create a new wallet', async (taskArgs, hre) => {
+  const wallet = await hre.ethers.Wallet.createRandom()
 
   console.log(`
 Address: ${wallet.address}
 Mnemonic: ${wallet.mnemonic.phrase}  
-`);
-});
+`)
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.4",
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
@@ -44,7 +45,7 @@ const config: HardhatUserConfig = {
   },
   networks: {
     bsc_testnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
       accounts: {
         mnemonic: process.env.MNEMONIC,
       },
@@ -52,11 +53,16 @@ const config: HardhatUserConfig = {
   },
   gasReporter: {
     enabled: true,
-    currency: "USD",
+    currency: 'USD',
   },
   etherscan: {
     apiKey: process.env.BSCSCAN_API,
   },
-};
+  docgen: {
+    path: './docs',
+    clear: true,
+    runOnCompile: true,
+  },
+}
 
-export default config;
+export default config
